faaa9497a406d2fc7a57a4c317e1ffd20475bb3b
SethLK <sethlk2006@gmail.com>
2024-01-06 21:36:11 +0630

    Enought for 2nite

---
+++ server/routes/user.js
@@ -0,0 +1,10 @@
+const User = require("../model/User")
+const express = require("express")
+const Router = express.Router()
+
+Router.get("/instructor/:id", async (req, res)=>{
+    try {
+    } catch (error) {
+        
+    }
+})

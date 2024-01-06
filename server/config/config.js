6b78542b566d4ae8a62a84ba7696dacb47302e15
SethLK <sethlk2006@gmail.com>
2024-01-06 21:04:41 +0630

    Add little

--- server/config/config.js
+++ server/config/config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 module.exports = {
     secretKey: process.env.JWT_SECRET,
+    mongoKey: process.env.MONGO
 };
   
 // console.log(process.env.JWT_SECRET)
--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const config = require("../config/config");
 
-mongoose.connect('mongodb://127.0.0.1:27017/lms', {
+mongoose.connect(config.mongoKey, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: 'lms'

282768cfb71324bb1a265f8a66dc649b1a6603a8
SethLK <sethlk2006@gmail.com>
2024-01-11 20:07:14 +0630

    Error fixed

--- client/src/pages/course/CoursePage.jsx
+++ client/src/pages/course/CoursePage.jsx
@@ -1,27 +1,29 @@
 import NavBar from "../../components/navbar";
 import { useState, useEffect } from "react";
-import getParam from "../../myhooks/getParam";
+import { useParams } from "react-router-dom";
 
 export default function CoursePage() {
     const [course, setCourse] = useState({});
-    const url = getParam("whole");
+    const uri = location.pathname
+    console.log(uri)
 
     useEffect(() => {
         async function fetchData() {
             try {
-                const res = await fetch(`http://localhost:3000/api${url}`);
-                if(res.ok){
+                const res = await fetch(`http://localhost:3000/api${uri}`);
+                if (res.ok) {
                     const data = await res.json();
                     setCourse(data);
+                } else {
+                    console.error("Error fetching course data");
                 }
-                
             } catch (err) {
                 console.error("Error fetching course data", err);
             }
         }
 
         fetchData();
-    }, []); 
+    }, []);
 
     return (
         <>
@@ -31,7 +33,7 @@ export default function CoursePage() {
                     {course && (
                         <>
                             <h1>Title: {course.title}</h1>
-                            <p>Description: {course.description} </p>
+                            <p>Description: {course.description}</p>
                         </>
                     )}
                 </div>

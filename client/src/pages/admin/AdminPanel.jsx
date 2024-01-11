d4f1557a0599922b45862f5e5d19b84b7122f9dc
SethLK <sethlk2006@gmail.com>
2024-01-11 21:19:04 +0630

    Add Panels

---
+++ client/src/pages/admin/AdminPanel.jsx
@@ -0,0 +1,7 @@
+export default function AdminPanel(){
+    return(
+        <div className="body">
+            <h1>Admin Panel</h1>
+        </div>
+    )
+}
---
+++ client/src/pages/staff/StuffPanel.jsx
@@ -0,0 +1,7 @@
+export default function StuffPanel(){
+    return(
+        <div className="body">
+            <h1>Stuff Panel</h1>
+        </div>
+    )
+}
